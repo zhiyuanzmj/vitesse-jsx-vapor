39e93ae2b0b6b8dcc9640403ff257fac6a2c0a76
zhiyuanzmj <260480378@qq.com>
2025-06-02 08:58:49 +0800

    chore: fix typecheck

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,12 +1,16 @@
-declare interface Window {
-  // extend the window
-}
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-
-  const component: DefineComponent<object, object, any>
-  export default component
-}
-
 declare const defineVaporComponent = defineComponent
+
+declare module 'vue' {
+  interface HTMLAttributes {
+  }
+  interface ReservedProps {
+  }
+}
+
+declare module 'vue-router' {
+  interface RouterLinkProps {
+    title?: unknown
+  }
+}
+
+export {}
